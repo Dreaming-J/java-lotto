a6c5a99c8e3e767c9e317e384693797ed51a3cbe
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2023-01-03 13:32:23 +0900

    feat: 로또 번호 1~45 보장

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lotto {
+    private static final String WRONG_FORMAT_OF_LOTTO_NUMBER = "로또는 중복되지 않는 6개의 숫자로 이루어져야 합니다.";
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private final List<LottoNumber> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException(WRONG_FORMAT_OF_LOTTO_NUMBER);
+        }
+    }
+}
