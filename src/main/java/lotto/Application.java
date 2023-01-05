a6dcc63db5f3ae28fe6f14cdd5112896fb390215
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2023-01-05 17:57:48 +0900

    feat: 로또 발행 과정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,6 @@ import lotto.controller.MainController;
 public class Application {
     public static void main(String[] args) {
         MainController mainController = new MainController();
-        mainController.start();
+        mainController.issueLotto();
     }
 }
--- src/main/java/lotto/controller/MainController.java
+++ src/main/java/lotto/controller/MainController.java
@@ -1,13 +1,33 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
 import lotto.model.LottoResult;
+import lotto.util.RandomGenerator;
 import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainController {
 
-    InputView inputView = new InputView();
+    private final InputView inputView;
+    private final OutputView outputView;
+    private LottoResult result;
+    private List<Lotto> lottos;
 
-    public void start() {
-        LottoResult result = inputView.readCost();
+    public MainController() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        this.lottos = new ArrayList<>();
+    }
+
+    public void issueLotto() {
+        result = inputView.readCost();
+
+        for (int iter = 0; iter < result.count(); iter++) {
+            lottos.add(new Lotto(RandomGenerator.createLottoNumbers()));
+        }
+        outputView.printLottos(result.count(), lottos);
     }
 }
